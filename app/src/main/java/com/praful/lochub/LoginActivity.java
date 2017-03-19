7a01987eb618681943f0df64fb33d51bc0b87dbd
Praful <prafulmishra0203@gmail.com>
2017-03-19 17:57:09 +0530

    Changed layout of loginActivity

--- app/src/main/java/com/praful/lochub/LoginActivity.java
+++ app/src/main/java/com/praful/lochub/LoginActivity.java
@@ -107,6 +107,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             finish();
                             startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         }
+                        else
+                            Toast.makeText(LoginActivity.this, "Incorrect Username or password!", Toast.LENGTH_SHORT).show();
                     }
                 });
 
